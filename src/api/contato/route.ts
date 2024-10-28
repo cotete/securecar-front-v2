81d2d12228e73469edd5ebf037435999daba012b
cotete <feliperibeirots@gmail.com>
2024-10-28 15:48:46 -0300

    feito os gets e posts das apis endereco contato e usuario

---
+++ src/api/contato/route.ts
@@ -0,0 +1,41 @@
+
+import { contato } from "@/components/LoginComponentes/FormRegistro/FormRegistro";
+import { NextResponse } from "next/server";
+
+export type contatoFinal={
+    id_contato:number
+} & contato
+
+export async function GET() {
+    try {
+        const res = await fetch(`Link-API`);
+        const data: contato[] = await res.json(); 
+        return data;
+    } catch (error) {
+        console.log("erro ao buscar contatos", error);
+        throw error; 
+    }
+}
+
+export async function POST(request:Request) {
+    try {
+        const contato: contato = await request.json();
+
+        const res = await fetch("contato-Link-java",{
+            method: "POST",
+            headers:{
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify(contato),
+        })
+        if(!res.ok){
+            throw new Error("Erro ao guardar contato")
+        }
+        const data : contatoFinal = await res.json()
+
+        return NextResponse.json(data)
+    } catch (error) {
+        console.log("erro ao guardar contato", error);
+        throw error; 
+    }
+}
---
+++ src/api/usuario/route.ts
@@ -0,0 +1,44 @@
+
+
+import { Usuario } from "@/app/login/page";
+import { NextResponse } from "next/server";
+
+
+export type FinalUser = {
+    id_user:number
+} & Usuario
+
+
+export async function GET() {
+    try {
+        const res = await fetch(`Link-API`);
+        const data: Usuario[] = await res.json(); 
+        return data;
+    } catch (error) {
+        console.log("erro ao buscar contatos", error);
+        throw error; 
+    }
+}
+
+export async function POST(request:Request) {
+    try {
+        const usuario: Usuario = await request.json();
+
+        const res = await fetch("usuario-Link-java",{
+            method: "POST",
+            headers:{
+            "Content-Type": "application/json",
+            },
+            body: JSON.stringify(usuario),
+        })
+        if(!res.ok){
+            throw new Error("Erro ao guardar usuario")
+        }
+        const data : FinalUser = await res.json()
+
+        return NextResponse.json(data)
+    } catch (error) {
+        console.log("erro ao guardar usuario: ", error);
+        throw error; 
+    }
+}
