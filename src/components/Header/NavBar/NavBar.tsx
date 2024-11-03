8b2eb3125d991161f437006eadae216210edb176
cotete <feliperibeirots@gmail.com>
2024-11-03 20:40:35 -0300

    ajustado o link do chatbot

--- src/components/Header/NavBar/NavBar.tsx
+++ src/components/Header/NavBar/NavBar.tsx
@@ -9,7 +9,7 @@ const NavBar = ({ isMenuHamburguer, onLinkClick  }: NavBarProps) => {
 
     return (
         <div className={`${isMenuHamburguer ? ' flex flex-col gap-2 w-full gap-y-5 celular:items-end' : 'tablet:hidden'}`}>
-            <Link className="text-xl text-white hover:text-black mx-3 font-medium" onClick={onLinkClick} href="/chatbot">ChatBot</Link>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium" onClick={onLinkClick} href="https://web-chat.global.assistant.watson.appdomain.cloud/preview.html?region=us-south&integrationID=73f9e0d6-ba50-48e3-9875-183d96deb3cb&serviceInstanceID=f8bcc4dd-e927-4915-8abd-ed33d159b5bf">ChatBot</Link>
             <Link className="text-xl text-white hover:text-black mx-3 font-medium " onClick={onLinkClick} href="/ajuda">Perguntas Frequentes</Link>
             <Link className="text-xl text-white hover:text-black mx-3 font-medium" onClick={onLinkClick} href="/blog">Novidades</Link>
             <Link className="text-xl text-white hover:text-black mx-3 font-medium" onClick={onLinkClick} href={"/integrantes"}> Equipe </Link>
