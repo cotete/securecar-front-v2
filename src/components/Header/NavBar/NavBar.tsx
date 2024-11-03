671e65baa569fd9e6c012154aa992de65c16e71d
cotete <feliperibeirots@gmail.com>
2024-11-03 17:43:00 -0300

    ajustado o menu hamburguer pra fechar quando clicado em um link

--- src/components/Header/NavBar/NavBar.tsx
+++ src/components/Header/NavBar/NavBar.tsx
@@ -2,16 +2,17 @@ import Link from "next/link"
 
 type NavBarProps = {
     isMenuHamburguer?: boolean
+    onLinkClick?: () => void;
 }
 
-const NavBar = ({ isMenuHamburguer }: NavBarProps) => {
+const NavBar = ({ isMenuHamburguer, onLinkClick  }: NavBarProps) => {
 
     return (
         <div className={`${isMenuHamburguer ? ' flex flex-col gap-2 w-full gap-y-5 celular:items-end' : 'tablet:hidden'}`}>
-            <Link className="text-xl text-white hover:text-black mx-3 font-medium" href="/chatbot">ChatBot</Link>
-            <Link className="text-xl text-white hover:text-black mx-3 font-medium " href="/ajuda">Perguntas Frequentes</Link>
-            <Link className="text-xl text-white hover:text-black mx-3 font-medium" href="/blog">Novidades</Link>
-            <Link className="text-xl text-white hover:text-black mx-3 font-medium" href={"/integrantes"}> Equipe </Link>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium" onClick={onLinkClick} href="/chatbot">ChatBot</Link>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium " onClick={onLinkClick} href="/ajuda">Perguntas Frequentes</Link>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium" onClick={onLinkClick} href="/blog">Novidades</Link>
+            <Link className="text-xl text-white hover:text-black mx-3 font-medium" onClick={onLinkClick} href={"/integrantes"}> Equipe </Link>
         </div>
     )
 }
