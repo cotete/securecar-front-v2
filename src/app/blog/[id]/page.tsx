a84df6d9d3f43a8e9fc444f89992a899cfc65ef9
gustavodscruz <gustavodiasdsc@gmail.com>
2024-11-04 16:58:02 -0300

    post blog

--- src/app/blog/[id]/page.tsx
+++ src/app/blog/[id]/page.tsx
@@ -1,116 +1,57 @@
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import TagPost from "@/components/BlogComponents/Post/TagPost/TagPost";
 import { ListaPostsBlog } from "@/components/BlogComponents/lista_posts_blog";
 import { notFound } from "next/navigation";
+import { PostProps } from "@/components/BlogComponents/Post/Post";
 
-export type PostProps = {
-  isMainPost?: boolean;
-  titulo: string;
-  dataCriacao: string;
-} & imagemPostProps & authorPostProps & tagPostProps;
-
-export type imagemPostProps = {
-  imagem: string | StaticImageData;
-  descImagem?: string;
-};
-
-export type authorPostProps = {
-  autor: string;
-  autorImagem: string;
-};
-
-export type tagPostProps = {
-  tagPost: string;
-};
+export type SinglePost = {
+  descricao: string;
+  conteudo: string;
+} & PostProps;
 
 interface PageProps {
   params: {
-    id: number;
+    id: string;
   };
 }
 
-const Post = ({
-  isMainPost,
-  imagem,
-  titulo,
-  autorImagem,
-  autor,
-  dataCriacao,
-  descImagem,
-  tagPost,
-}: PostProps) => {
-  return (
-    <>
-      {isMainPost ? (
-        <div className="w-full bg-white celular:flex celular:flex-col celular:justify-center celular:items-center" data-id={isMainPost}>
-          <Image
-            className="celular:w-[90vw] celular:h-[400px] w-full h-[600px] object-cover rounded-xl m-auto"
-            src={imagem}
-            alt={descImagem ?? `Foto sobre ${titulo}`}
-            height={600}
-            width={1000}
-          />
-          <div className="celular:w-[80vw] celular:ml-0 celular:-mt-10 relative bg-white p-10 flex flex-col w-2/5 justify-start -mt-60 ml-24 h-max rounded-xl shadow-md mb-4 hover:cursor-pointer hover:scale-105 transition-all duration-500">
-            <TagPost tagPost={tagPost} />
-            <h2 className="text-black celular:text-2xl text-4xl font-semibold py-4">{titulo}</h2>
-            <div className="post-details flex gap-3 justify-start py-4 items-center w-full">
-              <div className="flex gap-2 items-center">
-                <Image
-                  className="size-9 rounded-full object-cover object-top"
-                  src={autorImagem}
-                  alt={autor}
-                  height={36}
-                  width={36}
-                />
-                <p className="text-[16px] font-semibold text-gray-500">{autor}</p>
-              </div>
-              <div className="flex gap-2">
-                <p className="text-[14px] font-medium text-gray-400">{dataCriacao}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <div className="bg-white border border-[#c5c5c6] p-4 rounded-xl min-h-96 max-w-[400px] m-auto flex flex-col justify-center items-center hover:scale-105 hover:cursor-pointer transition-all duration-500">
-          <Image
-            className="h-[240px] w-[360px] rounded object-cover my-2 object-top"
-            src={imagem}
-            alt={descImagem ?? `Foto sobre ${titulo}`}
-            height={240}
-            width={360}
-          />
-          <TagPost tagPost={tagPost} />
-          <h3 className="text-black text-2xl font-semibold py-4">{titulo}</h3>
-          <div className="post-details flex gap-3 justify-between py-4 items-center w-full">
-            <div className="flex gap-2 items-center">
-              <Image
-                className="size-9 rounded-full object-cover object-top"
-                src={autorImagem}
-                alt={autor}
-                height={36}
-                width={36}
-              />
-              <p className="text-[16px] font-semibold text-gray-500">{autor}</p>
-            </div>
-            <div className="flex gap-2">
-              <p className="text-[14px] font-medium text-gray-400">{dataCriacao}</p>
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
-};
-
-const Page = ({ params }: PageProps) => {
-  const { id } = params;
-  const post = ListaPostsBlog.find((item) => item.idPost === id);
+const SinglePost = ({ params }: PageProps) => {
+  const idPost = Number(params.id);
+  const post = ListaPostsBlog.find((p) => p.idPost === idPost);
 
   if (!post) {
     notFound();
   }
 
-  return <Post {...post} isMainPost={true} />;
+  return (
+    <div className="max-w-screen-lg flex flex-col gap-5 m-auto celular:pt-0 py-5 ">
+      <Image
+        className="w-full object-cover max-h-[500px] rounded object-top"
+        src={post.imagem}
+        alt={post.descImagem ?? ""}
+        height={500}
+        width={1280}
+      />
+      <TagPost tagPost={post.tagPost} />
+      <h1 className="celular:px-[1.5em] text-2xl font-bold">{post.titulo}</h1>
+      <h4 className="celular:px-[2em] text-lg font-light">{post.descricao}</h4>
+      <div className="celular:px-[2em] flex gap-3 justify-start items-center">
+        <Image
+          className="h-10 w-10 object-cover rounded-full"
+          src={post.autorImagem}
+          alt={post.autor}
+          height={40}
+          width={40}
+        />
+        <p className="text-sm font-semibold text-gray-500">{post.autor}</p>
+        <p className="text-sm font-semibold text-gray-500">{post.dataCriacao}</p>
+      </div>
+      <div
+        className="text-black text-lg leading-snug gap-4 *:leading-snug group-[h3]:font-semibold"
+        dangerouslySetInnerHTML={{ __html: post.conteudo }}
+      />
+    </div>
+  );
 };
 
-export default Page;
+export default SinglePost;
